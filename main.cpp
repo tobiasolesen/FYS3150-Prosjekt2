5d8f083b5a5d47707550126493bfdff32663beac
tobiasolesen <31312131+tobiasolesen@users.noreply.github.com>
2017-09-16 13:16:47 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <cmath>
+//#include "jacobi.h"
+
+
 
 using namespace std;
 
 void printMatrixA(double** A, int N);
 
 int main(){
+    //Definerer var som trengs for a lage matrisen A
     int N = 5;
     double rho_0 = 0;
     double rho_max = 5;
@@ -12,7 +17,15 @@ int main(){
     double hh = h*h;
     double e = -1/hh;
 
-    //Lager vektoren rho:
+    //Definerer var som trengs for Jacobi-metoden:
+    int k, l;
+    double epsilon = 1.0e-8;
+    double max_number_iterations = (double) N * (double) N * (double) N;
+    int iterations = 0;
+    //double max_offdiag = maxoffdiag(A, &k, &l, N);
+
+
+    //Lager vektorer som trengs:
     double* rho = new double[N];
     double* V = new double[N];
     double* d = new double[N];
@@ -59,3 +72,33 @@ void printMatrixA(double** A, int N) {
     cout << endl;
     fflush(stdout);
 }
+
+//Setting up the eigenvector matrix:
+void jacobi_method(double** A, double** R, int N){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            if (i == j){
+                R[i][j] = 1.0;
+            }
+            else{
+                R[i][j] = 0.0;
+            }
+        }
+    }
+}
+
+//Function to find the maximum element:
+double maxoffdiag ( double ** A, int * k, int * l, int n ){
+    double max = 0.0;
+    for ( int i = 0; i < n; i++ ) {
+        for ( int j = i + 1; j < n; j++ ) {
+            if ( fabs(A[i][j]) > max ){
+                max = fabs(A[i][j]);
+                *l = i;
+                *k = j;
+            }
+        }
+    }
+return max;
+}
+
