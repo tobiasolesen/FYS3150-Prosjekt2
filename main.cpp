8c5afbad15ac9217d665518d26597083cd2d1723
tobiasolesen <31312131+tobiasolesen@users.noreply.github.com>
2017-09-21 15:52:44 +0200

    Add files via upload
    Testfilen, fungerer!

--- main.cpp
+++ main.cpp
@@ -2,24 +2,24 @@
 //Ikke i kallet pa funksjonen. Trenger da ikke bruke *k noe sted tror jeg.
 //Matriser skal vaere dobbeltpekere.
 //Vektorer skal vaere enkeltpekere.
-
+//Vet at de 3 laveste egenverdiene skal vaere: 3, 7, 11
 
 #include <iostream>
 #include <cmath>
+#include <array>
+#include <vector>
 //#include "jacobi.h"
 using namespace std;
 
 //Function prototypes:
 void printMatrixA(double** A, int N);
 double maxoffdiag(double** A, int& k, int& l, int N);
-//void jacobi_method(double** A, double** R, int N);
+void jacobi_method(double** A, double** R, int N);
 void rotate ( double ** A, double ** R, int k, int l, int N );
 
 int main(){
     //Definerer var som trengs for a lage matrisen A
-    int N = 5;
-    //int k;
-    //int l;
+    int N = 200;
     double rho_0 = 0;
     double rho_max = 5;
     double h = (rho_max - rho_0)/((double)N);
@@ -32,16 +32,18 @@ int main(){
     double* d = new double[N];
 
     //Fyller vektorene
-   for (int i = 0; i < N; i++){
-        rho[i] = rho_0 + i*h;
+    for (int i = 0; i < N; i++){
+        rho[i] = rho_0 + (i+1)*h;
         V[i] = rho[i]*rho[i];
         d[i] = 2./hh + V[i];
-   }
+    }
 
-    //Lager matrisen A:
+    //Deklarerer/lager matrisene A og R:
     double** A = new double*[N];
+    double** R = new double*[N];
     for (int i = 0; i < N; i++){
         A[i] = new double[N];
+        R[i] = new double[N];
     }
 
     //Fyller inn offdiag-elementene i A:
@@ -50,7 +52,7 @@ int main(){
             if (i == j-1){
                 A[i][j] = e;
             } else if (i == j+1){
-                    A[i][j] = e;
+                A[i][j] = e;
             } else {
                 A[i][j] = 0.0;
             }
@@ -58,10 +60,12 @@ int main(){
     }
     //Fyller diagonalelementene i A:
     for (int i = 0; i < N; i++){
-            A[i][i] = d[i];
+        A[i][i] = d[i];
     }
 
-    printMatrixA(A, N);
+    //printMatrixA(A, N);  //Skriver ut matrisen A som vi starter med
+
+    jacobi_method(A, R, N);
 
     return 0;
 }  //Slutt pa main
@@ -78,8 +82,9 @@ void printMatrixA(double** A, int N) {
     fflush(stdout);
 }
 
-//Setting up the eigenvector matrix:
+//Setting up the eigenvector matrix (som identitetsmatrisen?):
 void jacobi_method(double** A, double** R, int N) {
+
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
             if (i == j){
@@ -101,13 +106,27 @@ void jacobi_method(double** A, double** R, int N) {
         max_offdiag = maxoffdiag ( A, k, l, N );
         rotate ( A, R, k, l, N );
         iterations++;
+        //Hopper ut av denne lokken nar offdiag elementene til A er essentially zero
     }
+
     cout << "Number of iterations: " << iterations << "\n";
+
+    vector<double>diag(N);  //Deklarerer vektor diag med lengde N?
+
+    for (int i = 0; i < N; i++){
+        diag[i] = A[i][i];  //Fyller diag med A sine diagonalelementer
+    }
+
+    sort(diag.begin(), diag.end());  //Sorterer vektoren fra minst til storste verdier
+    //Skriver ut diag sine 3 forste elementer (3 forste egenverdiene):
+    cout << "3 forste egenverdier: " << diag[0] << " " << diag[1] << " " << diag[2] << endl;
+
     return;
 }
 
 //Function to find the maximum element of A:
 double maxoffdiag ( double** A, int& k, int& l, int N) {
+
     double max = 0.0;
     for ( int i = 0; i < N; i++ ) {
         for ( int j = i + 1; j < N; j++ ) {
@@ -118,33 +137,45 @@ double maxoffdiag ( double** A, int& k, int& l, int N) {
             }
         }
     }
-return max;
+
+    return max;
+
 }
 
 // Function to find the values of cos and sin
 void rotate ( double ** A, double ** R, int k, int l, int N ){
+
     double s, c;
+    //cout << k << " " << l << endl;
+
     if ( A[k][l] != 0.0 ) {
+
         double t, tau;
         tau = (A[l][l] - A[k][k])/(2*A[k][l]);
+
         if ( tau > 0 ) {
             t = 1.0/(tau + sqrt(1.0 + tau*tau));
         }
         else {
             t = -1.0/( -tau + sqrt(1.0 + tau*tau));
         }
-    c = 1/sqrt(1+t*t);
-    s = c*t;
-    }
-    else{
+        c = 1.0/sqrt(1.0+t*t);
+        s = c*t;
+    } else{
         c = 1.0;
         s = 0.0;
     }
+
     double a_kk, a_ll, a_ik, a_il, r_ik, r_il;
     a_kk = A[k][k];
     a_ll = A[l][l];
+
     // changing the matrix elements with indices k and l A[k][k] = c*c*a_kk - 2.0*c*s*A[k][l] + s*s*a_ll; A[l][l] = s*s*a_kk + 2.0*c*s*A[k][l] + c*c*a_ll; A[k][l] = 0.0; // hard-coding of the zeros
+
+    A[k][k] = a_kk*c*c - 2*A[k][l]*c*s + a_ll*s*s;
+    A[l][l] = a_ll*c*c + 2*A[k][l]*c*s + a_kk*s*s;
     A[l][k] = 0.0;
+    A[k][l] = 0.0;
     // and then we change the remaining elements
     for ( int i = 0; i < N; i++ ) {
         if ( i != k && i != l ){
@@ -163,4 +194,5 @@ void rotate ( double ** A, double ** R, int k, int l, int N ){
     }
 
     return;
+
 }
