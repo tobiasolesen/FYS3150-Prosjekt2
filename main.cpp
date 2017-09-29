7daf52f9c45cfd5f6893c19aa89e6380f7bdffa3
tobiasolesen <31312131+tobiasolesen@users.noreply.github.com>
2017-09-29 16:54:13 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 //Ikke i kallet pa funksjonen. Trenger da ikke bruke *k noe sted tror jeg.
 //Matriser skal vaere dobbeltpekere.
 //Vektorer skal vaere enkeltpekere.
-//Vet at de 3 laveste egenverdiene skal vaere: 3, 7, 11
+//Vet at de 3 laveste egenverdiene skal vaere: 3, 7, 11 (i b) )
 
 //i d) skal jeg bruke koden fra b), endrer bare pa potensialet V(rho)
 //rho er avstanden mellom partiklene (?)
 //Vil finne den minste egenverdien (tilsvarer energien til ground state) og tilhorende egenvektor (bolgefunksjonen i ground state)
+
+//Sammenligner energien vi faar for grunntilstand ( energi=1.25) med n=2-tilfellet i artikkelen, altsa energi=0.625 (hvor de har w_r=0.25).
+//Vi far dobbelt saa mye energi (1.25 = 2*0.625).
+//Dette er pa grunn av skaleringen av Schrodinger ligningen. Siden de beholder faktoren 1/2 i SL sa er det riktig at jeg faar dobbelt saa mye.
+
 //apne tekstfil i terminal: less "filnavn"
 //Komme ut av fil og tilbake i terminal: shift + q
 //Med w_r=0.25 skal de minste egenverdiene bli: 1.25, 2.19, 3.18 ca
+//Normen er bevart under similarity transform
+// 10.986 rundes opp sa har 4 leading digits accuracy to 11?
 
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
@@ -18,17 +25,90 @@
 #include <array>
 #include <vector>
 #include <fstream>
+#include <armadillo>
 //#include "jacobi.h"
 
 using namespace std;
+using namespace arma;
 
 //Function prototypes:
 void printMatrixA(double** A, int N);
 double maxoffdiag(double** A, int& k, int& l, int N);
-vector<int> jacobi_method(double** A, double** R, int N);  //returneren en vektor med doubles
+vector<int> jacobi_method(double** A, double** R, int N);  //returnerer en vektor med ints
 void rotate ( double ** A, double ** R, int k, int l, int N );
 
-int main(){
+//Unit test (skal sjekke om jacobi finner rett egenverdier):
+TEST_CASE("5x5 egenverdi-test") {
+    int n = 5;
+    double tolerance = pow(10.0, -2.0);
+    double eigenvalue0_known = 1;
+    double eigenvalue0;
+    vector <int> idx(n);
+    double eigval;
+    vector <double> eigvec;
+
+    //Set up 5x5 identity matrix:
+    double** testMatrix_1 = new double*[n];
+    for (int i = 0; i < n; i++){
+        testMatrix_1[i] = new double[n];
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (i == j){
+                testMatrix_1[i][j] = 1.0;
+            }
+            else{
+                testMatrix_1[i][j] = 0.0;
+            }
+        }
+    }
+
+    double** R = new double*[n];
+    for (int i = 0; i < n; i++){
+        R[i] = new double[n];
+    }
+
+    // solve with jacobi:
+    idx = jacobi_method(testMatrix_1, R, n);  //idx er vektor som inneholder indeksene til de 3 laveste egenverdiene
+    eigenvalue0 = testMatrix_1[idx[0]][idx[0]];  //Den laveste egenverdien
+    REQUIRE(abs(eigenvalue0 - eigenvalue0_known) < tolerance); //Krever at eigenvalue0 skal vaere naerme nok eigenvalue0_known
+
+}//Slutt test
+
+//Unit test (skal sjekke om maxoffdiag faktisk finner det storste offdiag elementet):
+TEST_CASE("5x5 maks offdiag element test") {
+    int n = 5;
+    double tolerance = pow(10.0, -2.0);
+    double maks_known = 0;
+    int k;
+    int l;
+
+    //Set up 5x5 matrix with:
+    double** testMatrix_2 = new double*[n];
+    for (int i = 0; i < n; i++){
+        testMatrix_2[i] = new double[n];
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (i == j){
+                testMatrix_2[i][j] = 1.0;
+            }
+            else{
+                testMatrix_2[i][j] = 0.0;
+            }
+        }
+
+    }
+    // use maxoffdiag:
+    double maks = maxoffdiag(testMatrix_2, k, l, n);
+    cout << "maks offdiag element: " << maks << endl;
+
+    REQUIRE(abs(maks - maks_known) < tolerance);
+}//Slutt test
+
+int main(int argc, char* argv[]){
+
+    int result = Catch::Session().run(argc, argv);  //kjorer testene
 
     //Definerer var som trengs for a lage matrisen A
     //N = 200 er passe?
@@ -47,6 +127,8 @@ int main(){
     double* V = new double[N];
     double* d = new double[N];
     double* eigenvec0 = new double[N];
+    //double* eigenvalues = new double[N];
+    vec eigval; // armadillo vektor
 
     //Fyller vektorene
     for (int i = 0; i < N; i++){
@@ -79,11 +161,37 @@ int main(){
     for (int i = 0; i < N; i++){
         A[i][i] = d[i];
     }
+
+
+
     //printMatrixA(A, N);  //Skriver ut matrisen A som vi starter med
 
-    vector <int> ind = jacobi_method(A, R, N); //Kaller jacobi_method. ind inneholder
+    //Maa definere B som en armadillo-matrise for a kunne bruke eig_sym
+    mat B(N, N);
+    for (int i = 0; i < N; i++){   //Fyller inn B slik at den blir lik som A
+        for (int j = 0; j < N; j++){
+            if (i == j-1){
+                B(i,j) = e;
+            } else if (i == j+1){
+                B(i,j) = e;
+            } else {
+                B(i,j) = 0.0;
+            }
+        }
+    }
 
-    //Skriver til filen data.dat
+    for (int i = 0; i < N; i++){
+        B(i, i) = d[i];
+    }
+
+    eig_sym(eigval, B);//Bruker funksjonen eig_sym som lagrer egenverdiene i vektoren eigval
+    eigval = sort(eigval); //Sorterer elementene i eigval fra minst til storst
+    eigval(span(0,2)).print("De tre laveste egenverdiene fra eigsym:"); //Skriver ut elementene 0, 1 og 2 i eigval
+    //eigval.print("Armadillo eigval: ");  //Skriver ut alle egenverdiene i eigval
+
+    vector <int> ind = jacobi_method(A, R, N); //Kaller jacobi_method. ind inneholder naa indeksene til egenverdiene
+
+    //Skriver til filen egenverdier.dat
     ofstream outFile;
     outFile.open("egenverdier.dat", std::ios::out);
 
@@ -137,14 +245,14 @@ vector<int> jacobi_method(double** A, double** R, int N) {
 
     cout << "Number of iterations: " << iterations << "\n";
 
-    vector<double>diag(N);  //Deklarerer vektor diag med lengde N?
+    vector<double>diag(N);  //Deklarerer vektor diag med lengde N
 
     for (int i = 0; i < N; i++){
         diag[i] = A[i][i];  //Fyller diag med A sine diagonalelementer
     }
 
     //finner indeks til minste egenverdiene og lagrer indeksen i lowestIndex
-    int ll = 3;  //Antall egenverdier jeg bryr meg om
+    int ll = 3;  //ll (3) er antallet egenverdier jeg bryr meg om
     vector<double> lowest(ll);
     vector<int> lowestIndex(ll);  //vektor som skal inneholde indeksene til de minste egenverdiene
     for (int i = 0; i < ll; i++) {
@@ -163,15 +271,19 @@ vector<int> jacobi_method(double** A, double** R, int N) {
     }
 
     //printMatrixA(A,N);
+
+    cout << "De tre laveste egenverdiene fra Jacobi-metoden:" << endl;
     for (int i = 0; i < ll; i++) {
-        cout <<  "Laveste egenverdier: " << lowest[i] << " tilhorende index: " << lowestIndex[i] << endl;
+        cout << lowest[i] << " tilhorende index: " << lowestIndex[i] << endl;
     }
 
     sort(diag.begin(), diag.end());  //Sorterer vektoren fra minst til storste verdier
     //Skriver ut diag sine 3 forste elementer (3 forste egenverdiene):
+    //cout << "3 forste egenverdier: " << diag[lowestIndex[0]] << " " << diag[1] << " " << diag[2] << endl;
     //cout << "3 forste egenverdier: " << diag[0] << " " << diag[1] << " " << diag[2] << endl;
 
-    return lowestIndex;
+
+    return lowestIndex;  //Inneholder indeksene til de 3 laveste egenverdiene
 }
 
 //Function to find the maximum element of A:
